b504ece2465dcf66682394b6f26b42e02ecc4bc4
Markus <mysecondworld@firesurf.de>
2021-11-25 22:18:26 +0100

    Detect previous session.

--- jesse/modes/optimize_mode/__init__.py
+++ jesse/modes/optimize_mode/__init__.py
@@ -4,14 +4,14 @@ from math import log10
 from multiprocessing import cpu_count
 
 import click
-from ray import tune
-import ray
 import numpy as np
-from ray.tune.suggest.optuna import OptunaSearch
+import ray
 from optuna.samplers import NSGAIISampler
+from ray import tune
+from ray.tune.suggest.optuna import OptunaSearch
+import shutil
 
 import jesse.helpers as jh
-import jesse.services.logger as logger
 import jesse.services.required_candles as required_candles
 from jesse import exceptions
 from jesse.config import config
@@ -22,6 +22,7 @@ from jesse.services.validators import validate_routes
 from jesse.store import store
 
 os.environ['NUMEXPR_MAX_THREADS'] = str(cpu_count())
+
 
 class Optimizer():
     def __init__(self, training_candles, optimal_total: int, cpu_cores: int, iterations: int) -> None:
@@ -63,62 +64,57 @@ class Optimizer():
 
         self.study_name = f'{self.strategy_name}-{self.exchange}-{self.symbol}-{self.timeframe}'
 
-
     def objective_function(self, search_space):
         score = np.nan
-        try:
-            # init candle store
-            store.candles.init_storage(5000)
-            # inject required TRAINING candles to the candle store
 
-            for num, c in enumerate(config['app']['considering_candles']):
-                required_candles.inject_required_candles_to_store(
-                    self.training_initial_candles[num],
-                    c[0],
-                    c[1]
-                )
-            # run backtest simulation
-            simulator(self.training_candles, search_space)
+        # init candle store
+        store.candles.init_storage(5000)
+        # inject required TRAINING candles to the candle store
 
-            training_data = stats.trades(store.completed_trades.trades, store.app.daily_balance)
-            total_effect_rate = log10(training_data['total']) / log10(self.optimal_total)
-            total_effect_rate = min(total_effect_rate, 1)
-            ratio_config = jh.get_config('env.optimization.ratio', 'sharpe')
-            if ratio_config == 'sharpe':
-                ratio = training_data['sharpe_ratio']
-                ratio_normalized = jh.normalize(ratio, -.5, 5)
-            elif ratio_config == 'calmar':
-                ratio = training_data['calmar_ratio']
-                ratio_normalized = jh.normalize(ratio, -.5, 30)
-            elif ratio_config == 'sortino':
-                ratio = training_data['sortino_ratio']
-                ratio_normalized = jh.normalize(ratio, -.5, 15)
-            elif ratio_config == 'omega':
-                ratio = training_data['omega_ratio']
-                ratio_normalized = jh.normalize(ratio, -.5, 5)
-            elif ratio_config == 'serenity':
-                ratio = training_data['serenity_index']
-                ratio_normalized = jh.normalize(ratio, -.5, 15)
-            elif ratio_config == 'smart sharpe':
-                ratio = training_data['smart_sharpe']
-                ratio_normalized = jh.normalize(ratio, -.5, 5)
-            elif ratio_config == 'smart sortino':
-                ratio = training_data['smart_sortino']
-                ratio_normalized = jh.normalize(ratio, -.5, 15)
-            else:
-                raise ValueError(
-                    f'The entered ratio configuration `{ratio_config}` for the optimization is unknown. Choose between sharpe, calmar, sortino, serenity, smart shapre, smart sortino and omega.')
+        for num, c in enumerate(config['app']['considering_candles']):
+            required_candles.inject_required_candles_to_store(
+                self.training_initial_candles[num],
+                c[0],
+                c[1]
+            )
+        # run backtest simulation
+        simulator(self.training_candles, search_space)
 
-            if ratio > 0:
-                score = total_effect_rate * ratio_normalized
+        training_data = stats.trades(store.completed_trades.trades, store.app.daily_balance)
+        total_effect_rate = log10(training_data['total']) / log10(self.optimal_total)
+        total_effect_rate = min(total_effect_rate, 1)
+        ratio_config = jh.get_config('env.optimization.ratio', 'sharpe')
+        if ratio_config == 'sharpe':
+            ratio = training_data['sharpe_ratio']
+            ratio_normalized = jh.normalize(ratio, -.5, 5)
+        elif ratio_config == 'calmar':
+            ratio = training_data['calmar_ratio']
+            ratio_normalized = jh.normalize(ratio, -.5, 30)
+        elif ratio_config == 'sortino':
+            ratio = training_data['sortino_ratio']
+            ratio_normalized = jh.normalize(ratio, -.5, 15)
+        elif ratio_config == 'omega':
+            ratio = training_data['omega_ratio']
+            ratio_normalized = jh.normalize(ratio, -.5, 5)
+        elif ratio_config == 'serenity':
+            ratio = training_data['serenity_index']
+            ratio_normalized = jh.normalize(ratio, -.5, 15)
+        elif ratio_config == 'smart sharpe':
+            ratio = training_data['smart_sharpe']
+            ratio_normalized = jh.normalize(ratio, -.5, 5)
+        elif ratio_config == 'smart sortino':
+            ratio = training_data['smart_sortino']
+            ratio_normalized = jh.normalize(ratio, -.5, 15)
+        else:
+            raise ValueError(
+                f'The entered ratio configuration `{ratio_config}` for the optimization is unknown. Choose between sharpe, calmar, sortino, serenity, smart shapre, smart sortino and omega.')
 
-        except Exception as e:
-            logger.error("".join(traceback.TracebackException.from_exception(e).format()))
+        if ratio > 0:
+            score = total_effect_rate * ratio_normalized
 
         # reset store
         store.reset()
         tune.report(score=score)
-
 
     def get_search_space(self):
         config = {}
@@ -131,7 +127,9 @@ class Optimizer():
                 if 'step' not in st_hp:
                     st_hp['step'] = 0.1
                 decs = str(st_hp['step'])[::-1].find('.')
-                config[st_hp['name']] = tune.choice(np.trunc(np.arange(st_hp['min'], st_hp['max'] + st_hp['step'], st_hp['step']) * 10 ** decs) / (10 ** decs))
+                config[st_hp['name']] = tune.choice(
+                    np.trunc(np.arange(st_hp['min'], st_hp['max'] + st_hp['step'], st_hp['step']) * 10 ** decs) / (
+                                10 ** decs))
             elif st_hp['type'] is bool:
                 config[st_hp['name']] = tune.choice([True, False])
             else:
@@ -146,6 +144,19 @@ class Optimizer():
 
         sampler = NSGAIISampler(population_size=300, mutation_prob=0.0333, crossover_prob=0.6, swapping_prob=0.05)
 
+        local_dir = "./storage/genetics/ray"
+        resume = False
+
+        study_dir = local_dir+"/"+self.study_name
+        # if temp file exists, load data to resume previous session
+        if os.path.exists(study_dir) and click.confirm(
+                'Previous session detected. Do you want to resume?', default=True
+        ):
+            resume = True
+        else:
+            shutil.rmtree(study_dir, ignore_errors=True)
+
+
         optuna_search = OptunaSearch(
             search_space,
             sampler=sampler,
@@ -155,14 +166,19 @@ class Optimizer():
         analysis = tune.run(
             self.objective_function,
             search_alg=optuna_search,
-            num_samples=self.iterations
+            local_dir=local_dir,
+            num_samples=self.iterations,
+            name=self.study_name,
+            log_to_file=True,
+            max_concurrent_trials=self.cpu_cores,
+            resume=resume
         )
 
         print(f"Best config: {analysis.best_config}")
 
 
 def optimize_mode_ray(start_date: str, finish_date: str, optimal_total: int, cpu_cores: int,
-                              iterations: int) -> None:
+                      iterations: int) -> None:
     # clear the screen
     click.clear()
 
@@ -178,9 +194,7 @@ def optimize_mode_ray(start_date: str, finish_date: str, optimal_total: int, cpu
     optimizer.run()
 
 
-
 def get_training_candles(start_date_str: str, finish_date_str: str):
     # Load candles (first try cache, then database)
     from jesse.modes.backtest_mode import load_candles
     return load_candles(start_date_str, finish_date_str)
-
